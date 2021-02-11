b8c1825591620b3ffb9aa1fc2b356a6ba060a7b5
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-02-11 19:02:31 -0300

    add react-router-dom

---
+++ frontend-app/src/routes.js
@@ -0,0 +1,15 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+const routes = [
+    {
+        path: "/components",
+        component:RestaurantMenu
+    }
+]
+
+export default routes;
