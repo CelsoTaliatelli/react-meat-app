9dd1b1d8036895fbff20bce8dd0dc7639608bfe8
CelsoTaliatelli <celsotaliatelli@hotmail.com>
2021-02-15 16:34:30 -0300

    add package axios

--- frontend-app/src/components/RestaurantList.js
+++ frontend-app/src/components/RestaurantList.js
@@ -1,32 +1,30 @@
 import { Container, Grid, makeStyles } from "@material-ui/core";
 import Restaurant from "./Restaurant";
-import { getAll } from '../Services/restaurants';
-import {
-    
-    Link
-  } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { axios } from 'axios';
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) =>({
-    cardGrid:{
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
         paddingTop: theme.spacing(15),
         paddingBottom: theme.spacing(8)
     }
 }));
 export default function RestaurantList() {
     const classes = useStyles();
-    const res = getAll();
-    const restaurants = [1,2,3,4,5,6];
+    const restaurants = [1, 2, 3];
+    const [state, setstate] = useState({rest: []});
     return (
-        <Link to="/restaurant" style={{textDecoration: 'none'}}>
         <Container className={classes.cardGrid} maxWidth="md">
             <Grid container spacing={4}>
-                {restaurants.map((r) => (
-                    <Grid item  key={r} xs={12} sm={6} md={4}>
-                        <Restaurant />
+            {restaurants.map((r) => (
+                    <Grid item key={r} xs={12} sm={6} md={4}>
+                        <Link to="/restaurant" style={{ textDecoration: 'none' }}>
+                            <Restaurant />
+                        </Link>
                     </Grid>
                 ))}
             </Grid>
-        </Container>
-        </Link>
+        </Container >
     );
 } 
