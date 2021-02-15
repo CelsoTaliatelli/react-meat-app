6e45b22c3881f6a9c32b1b7e848297324f86f81d
CelsoTaliatelli <celsotaliatelli@hotmail.com>
2021-02-15 12:11:47 -0300

    ajuste no retorno do FetchAPI

--- frontend-app/src/Services/restaurants.js
+++ frontend-app/src/Services/restaurants.js
@@ -2,6 +2,11 @@ import fetch from 'node-fetch'
 
 export async function getAll() {
     return fetch('http://localhost:3004/restaurants')
-        .then(response => response.json());
+        .then(response => response.json()
+        .then(function(data){
+            let res = data.map(function(item,index){
+                console.log(item);
+            })
+        }));
     
 }
